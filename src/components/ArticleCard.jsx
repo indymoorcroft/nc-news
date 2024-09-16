b184ca79758e5bfa43ac8ba1238a4cd6b454729c
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2024-09-16 15:40:48 +0100

    Task 5: View an individual article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const ArticleCard = ({ article }) => {
   const date = article.created_at.slice(0, 10);
   return (
@@ -9,6 +11,7 @@ export const ArticleCard = ({ article }) => {
         posted by: <b>{article.author}</b>
       </p>
       <p>date posted: {date}</p>
+      <Link to={`/articles/${article.article_id}`}>Learn more</Link>
     </li>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { UserProvider } from "./contexts/UserContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <UserProvider>
-    <App />
-  </UserProvider>
+  <BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </BrowserRouter>
 );
