ddf4be7872ed46c714859ce1864c662aeab0bb4c
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2024-09-17 14:19:41 +0100

    Task 10: View a separate page for each topic with a list of related articles

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 
-export const ArticleCard = ({ article }) => {
+export const ArticleCard = ({ article, searchParams, setSearchParams }) => {
+  const handleClick = ({ target: { name } }) => {
+    setSearchParams({ ...searchParams, topic: name });
+  };
+
   const date = article.created_at.slice(0, 10);
   return (
     <li className="articleCard">
       <h3>{article.title}</h3>
-      <button className="topic">{article.topic}</button>
+      <button className="topic" onClick={handleClick} name={article.topic}>
+        {article.topic}
+      </button>
       <img src={article.article_img_url} />
       <p>
         posted by: <b>{article.author}</b>
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,15 +1,20 @@
 import { useState, useEffect } from "react";
 import { getAllArticles } from "../apiCalls";
 import { ArticleCard } from "./ArticleCard";
+import { useSearchParams } from "react-router-dom";
 
 export const ArticleList = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const topicQuery = searchParams.get("topic");
+  const params = { params: { topic: topicQuery } };
 
   useEffect(() => {
     setIsLoading(true);
-    getAllArticles()
+    getAllArticles(params)
       .then(({ articles }) => {
         setIsLoading(false);
         setArticles(articles);
@@ -18,7 +23,7 @@ export const ArticleList = () => {
         setIsLoading(false);
         setIsError(true);
       });
-  }, []);
+  }, [topicQuery]);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -28,13 +33,27 @@ export const ArticleList = () => {
     return <p>Bad request</p>;
   }
 
+  const handleClick = () => {
+    setSearchParams();
+  };
+
   return (
     <section>
       <ul className="container">
         {articles.map((article) => {
-          return <ArticleCard key={article.article_id} article={article} />;
+          return (
+            <ArticleCard
+              key={article.article_id}
+              article={article}
+              searchParams={searchParams}
+              setSearchParams={setSearchParams}
+            />
+          );
         })}
       </ul>
+      {searchParams ? (
+        <button onClick={handleClick}>view all articles</button>
+      ) : null}
     </section>
   );
 };
